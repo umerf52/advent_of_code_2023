10447178b256bd63eba0e594422ccedbea8c6174
Umer Zia <umer.zia@tum.de>
2023-12-02 14:20:24 +0100

    Day 2

---
+++ day02/p1_solution.py
@@ -0,0 +1,32 @@
+REDS = 12
+GREENS = 13
+BLUES = 14
+
+total = 0
+
+with open('input_file.txt') as f:
+    game_id = 1
+    for line in f:
+        is_valid = True
+        line = line.split(":")[1]
+        line = line.replace(";", ",")
+        draws = line.split(", ")
+        draws = [draw.strip() for draw in draws]
+        for draw in draws:
+            count, color = int(draw.split(" ")[0]), draw.split(" ")[1]
+            count = int(count)
+            if color == "red" and count > REDS:
+                    is_valid = False
+                    break
+            elif color == "green" and count > GREENS:
+                    is_valid = False
+                    break
+            elif color == "blue" and count > BLUES:
+                    is_valid = False
+                    break
+
+        if is_valid:   
+            total += game_id
+        game_id += 1
+    
+print(total)
---
+++ day02/p2_solution.py
@@ -0,0 +1,21 @@
+total = 0
+
+with open('input_file.txt') as f:
+    for line in f:
+        max_counts = {
+            "red": 0,
+            "green": 0,
+            "blue": 0
+        }
+        line = line.split(":")[1]
+        line = line.replace(";", ",")
+        draws = line.split(", ")
+        draws = [draw.strip() for draw in draws]
+
+        for draw in draws:
+            count, color = int(draw.split(" ")[0]), draw.split(" ")[1]
+            max_counts[color] = max(max_counts[color], count)
+
+        total = total + max_counts["red"] * max_counts["green"] * max_counts["blue"]
+    
+print(total)
