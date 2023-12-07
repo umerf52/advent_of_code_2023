4e58374faa172b211ee08627627a47a0e0b6c523
Umer Zia <umer.zia@tum.de>
2023-12-07 23:31:45 +0100

    Day 5, part 1

--- day05/p1_solution.py
+++ day05/p1_solution.py
@@ -13,58 +13,71 @@ lines = lines[1:]
 
 groups = [list(g) for k, g in groupby(lines, key=bool) if k]
 
-seed_to_soil_map = dict()
-for line in groups[0][1:]:
-    data = line.split()
-    for offset in range(int(data[2])):
-        seed_to_soil_map[int(data[1]) + offset] = int(data[0]) + offset
-
-soil_to_fertilizer_map = dict()
-for line in groups[1][1:]:
-    data = line.split()
-    for offset in range(int(data[2])):
-        soil_to_fertilizer_map[int(data[1]) + offset] = int(data[0]) + offset
-
-fertilizer_to_water_map = dict()
-for line in groups[2][1:]:
-    data = line.split()
-    for offset in range(int(data[2])):
-        fertilizer_to_water_map[int(data[1]) + offset] = int(data[0]) + offset
-
-water_to_light_map = dict()
-for line in groups[3][1:]:
-    data = line.split()
-    for offset in range(int(data[2])):
-        water_to_light_map[int(data[1]) + offset] = int(data[0]) + offset
-
-light_to_temperature_map = dict()
-for line in groups[4][1:]:
-    data = line.split()
-    for offset in range(int(data[2])):
-        light_to_temperature_map[int(data[1]) + offset] = int(data[0]) + offset
-
-temperature_to_humidity_map = dict()
-for line in groups[5][1:]:
-    data = line.split()
-    for offset in range(int(data[2])):
-        temperature_to_humidity_map[int(data[1]) + offset] = int(data[0]) + offset
-
-humidity_to_location_map = dict()
-for line in groups[6][1:]:
-    data = line.split()
-    for offset in range(int(data[2])):
-        humidity_to_location_map[int(data[1]) + offset] = int(data[0]) + offset
-
 locations = []
+for result in seeds:
+    # seed to soil
+    for line in groups[0][1:]:
+        data = line.split()
+        dest, src, offset = int(data[0]), int(data[1]), int(data[2])
+        r = range(src, src + offset)
+        if result in r:
+            result = abs(result - src) + dest
+            break
 
-for seed in seeds:
-    soil = seed_to_soil_map.get(seed, seed)
-    fertilizer = soil_to_fertilizer_map.get(soil, soil)
-    water = fertilizer_to_water_map.get(fertilizer, fertilizer)
-    light = water_to_light_map.get(water, water)
-    temperature = light_to_temperature_map.get(light, light)
-    humidity = temperature_to_humidity_map.get(temperature, temperature)
-    location = humidity_to_location_map.get(humidity, humidity)
-    locations.append(location)
+    # soil to fertilizer
+    for line in groups[1][1:]:
+        data = line.split()
+        dest, src, offset = int(data[0]), int(data[1]), int(data[2])
+        r = range(src, src + offset)
+        if result in r:
+            result = abs(result - src) + dest
+            break
+
+    # fertilizer to water
+    for line in groups[2][1:]:
+        data = line.split()
+        dest, src, offset = int(data[0]), int(data[1]), int(data[2])
+        r = range(src, src + offset)
+        if result in r:
+            result = abs(result - src) + dest
+            break
+
+    # water to light
+    for line in groups[3][1:]:
+        data = line.split()
+        dest, src, offset = int(data[0]), int(data[1]), int(data[2])
+        r = range(src, src + offset)
+        if result in r:
+            result = abs(result - src) + dest
+            break
+
+    # light to temperature
+    for line in groups[4][1:]:
+        data = line.split()
+        dest, src, offset = int(data[0]), int(data[1]), int(data[2])
+        r = range(src, src + offset)
+        if result in r:
+            result = abs(result - src) + dest
+            break
+
+    # temperature to humidity
+    for line in groups[5][1:]:
+        data = line.split()
+        dest, src, offset = int(data[0]), int(data[1]), int(data[2])
+        r = range(src, src + offset)
+        if result in r:
+            result = abs(result - src) + dest
+            break
+
+    # humidity to location
+    for line in groups[6][1:]:
+        data = line.split()
+        dest, src, offset = int(data[0]), int(data[1]), int(data[2])
+        r = range(src, src + offset)
+        if result in r:
+            result = abs(result - src) + dest
+            break
+
+    locations.append(result)
 
 print(min(locations))
